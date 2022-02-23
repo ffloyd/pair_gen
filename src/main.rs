89b656e01b9959d53f5bec6763c821727cad6533
Roman Kolesnev <rkolesnev@newstore.com>
2022-02-23 18:06:54 +0100

    Basic `pop_distribution` has been implemented

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ type Map<K, V> = BTreeMap<K, V>;
 
 /// Represents set of available pairs.
 #[derive(Debug)]
-struct AvailablePairs<'a> {
-    data: Map<&'a str, Set<&'a str>>,
+struct AvailablePairs<T: Ord + Clone + Copy> {
+    data: Map<T, Set<T>>,
 }
 
-impl<'a> AvailablePairs<'a> {
+impl<T: Ord + Clone + Copy> AvailablePairs<T> {
     /// Create set of all combinations of `names`.
-    fn new(names: &[&'a str]) -> Self {
+    fn new(names: &[T]) -> Self {
         let mut data = Map::new();
         let sorted_names = {
             let mut mutable_names = names.to_vec();
@@ -21,7 +21,7 @@ impl<'a> AvailablePairs<'a> {
         };
 
         for &key_name in &sorted_names {
-            let greater_names: Set<&str> = sorted_names
+            let greater_names: Set<T> = sorted_names
                 .iter()
                 .skip_while(|&&value_name| value_name <= key_name)
                 .cloned()
@@ -41,10 +41,10 @@ impl<'a> AvailablePairs<'a> {
     }
 
     /// Returns a pair made from `allowed_names`.
-    fn get_pair(&self, allowed_names: &Set<&str>) -> Option<(&str, &str)> {
+    fn get_pair(&self, allowed_names: &Set<T>) -> Option<(T, T)> {
         self.data
             .iter()
-            .find(|(&key, _)| allowed_names.contains(key))
+            .find(|(key, _)| allowed_names.contains(key))
             .and_then(|(&first_name, set)| {
                 set.iter()
                     .find(|&name| allowed_names.contains(name))
@@ -55,24 +55,42 @@ impl<'a> AvailablePairs<'a> {
     /// Removes pair from set. Returns `true` if pair was found.
     ///
     /// `name_a` should be less than `name_b`.
-    fn remove_pair(&mut self, name_a: &str, name_b: &str) -> bool {
+    fn remove_pair(&mut self, name_a: T, name_b: T) -> bool {
         self.data
-            .get_mut(name_a)
-            .map(|set| set.remove(name_b))
+            .get_mut(&name_a)
+            .map(|set| set.remove(&name_b))
             .unwrap_or(false)
     }
 
     /// Returns a pair made from `allowed_names` and removes from set.
-    fn pop_pair(&mut self, allowed_names: &Set<&str>) -> Option<(&str, &str)> {
+    fn pop_pair(&mut self, allowed_names: &Set<T>) -> Option<(T, T)> {
         let maybe_pair = self.get_pair(allowed_names);
 
         if let Some((a, b)) = maybe_pair {
-            // self.remove_pair(a, b);
+            self.remove_pair(a, b);
         }
 
         maybe_pair
     }
+
+    /// Distributes `allowed_names` to available pairs.
+    ///
+    /// Excludes used names from `allowed_names`. Empty set will be returned if no single pair possible.
+    fn pop_distribution(&mut self, allowed_names: &mut Set<T>) -> Set<(T, T)> {
+        let mut result = Set::new();
+
+        while let Some(pair) = self.pop_pair(allowed_names) {
+            result.insert(pair);
+            let (a, b) = pair;
+            allowed_names.remove(&a);
+            allowed_names.remove(&b);
+        }
+
+        result
+    }
 }
+
+// 
 
 #[cfg(test)]
 mod tests {
@@ -144,6 +162,35 @@ mod tests {
         fn pop_pair_when_impossible() {
             let mut pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
             assert_eq!(pt.pop_pair(&Set::from(["Alice", "Jane"])), None);
+        }
+
+        #[test]
+        fn pop_distribution_when_possible() {
+            let people = ["Alice", "Bob", "Jane", "John"];
+            let mut pt = AvailablePairs::new(&people);
+            assert_eq!(
+                pt.pop_distribution(&mut Set::from(people)),
+                Set::from([
+                    ("Alice", "Bob"),
+                    ("Jane", "John")
+                ])
+            );
+        }
+
+        #[test]
+        fn pop_distribution_returns_unused() {
+            let people = ["Alice", "Bob", "Jane"];
+            let mut pt = AvailablePairs::new(&people);
+            let mut ppl_set = Set::from(people);
+
+            assert_eq!(
+                pt.pop_distribution(&mut ppl_set),
+                Set::from([
+                    ("Alice", "Bob")
+                ])
+            );
+
+            assert_eq!(ppl_set, Set::from(["Jane"]));
         }
     }
 }
