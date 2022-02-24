1563e5fb170ff747a2ab8dde8695b11ce0dbec9f
Roman Kolesnev <rkolesnev@newstore.com>
2022-02-24 14:01:54 +0100

    Draft of new algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,250 +1,102 @@
-use std::collections::BTreeMap;
-use std::collections::BTreeSet;
-use std::fmt::Debug;
+use std::collections::BTreeSet as Set;
 
-type Set<T> = BTreeSet<T>;
-type Map<K, V> = BTreeMap<K, V>;
+// ideas:
+//
+// simple recursive distribution generator is done
+// but we can do a more challenging task
+// to generate all possible distributions
+//
+// API can be the following:
+// let ctx = Context::new(names: &[T], desires: &[(T, T)], history: &[&[(T, T)]])
+// Context - contains pair distribution context.
+//
+// ctx.next_dist() -> Vec<(T, T)> - Returns next distribution.
+// ctx.all_next_dist() -> DistTree - Returns all possible distributions as a tree-like structure.
+// count_dists(DistTree)
 
-/// Represents set of available pairs.
-#[derive(Debug)]
-struct AvailablePairs<T: Ord + Clone + Copy + Debug> {
-    data: Map<T, Set<T>>,
+fn all_pairs<T: Clone + Ord + Copy>(names: Set<T>) -> Vec<(T, T)> {
+    let mut result = Vec::<(T, T)>::new();
+
+    let mut iter = names.iter();
+    while let Some(&left_name) = iter.next() {
+        iter.clone().for_each(|&right_name| {
+            result.push((left_name, right_name));
+        })
+    }
+
+    result
 }
 
-impl<T: Ord + Clone + Copy + Debug> AvailablePairs<T> {
-    /// Create set of all combinations of `names`.
-    fn new(names: &[T]) -> Self {
-        let mut data = Map::new();
-        let sorted_names = {
-            let mut mutable_names = names.to_vec();
-            mutable_names.sort_unstable();
-            mutable_names
-        };
+fn dist<T: Ord + Clone + Copy>(pairs: &[(T, T)], names: Set<T>) -> Option<Vec<(T, T)>> {
+    if names.is_empty() {
+        return Some(Vec::new());
+    }
+    
+    let mut iter = pairs.iter();
+    while let Some(pair) = iter.next() {
+        let (a, b) = pair;
+        if names.contains(a) && names.contains(b) {
+            let pairs_left = iter.clone().as_slice();
 
-        for &key_name in &sorted_names {
-            let greater_names: Set<T> = sorted_names
-                .iter()
-                .skip_while(|&&value_name| value_name <= key_name)
-                .cloned()
-                .collect();
-
-            if !greater_names.is_empty() {
-                data.insert(key_name, greater_names);
+            let mut next_names = names.clone();
+            next_names.remove(a);
+            next_names.remove(b);
+            
+            if let Some(mut tail) = dist(pairs_left, next_names) {
+                tail.push(*pair);
+                return Some(tail);
             }
         }
-
-        Self { data }
     }
 
-    /// Returns count of available pairs.
-    fn count(&self) -> usize {
-        self.data.iter().fold(0, |acc, (_, set)| acc + set.len())
-    }
-
-    /// Returns a pair made from `allowed_names`.
-    fn get_pair(&self, allowed_names: &Set<T>) -> Option<(T, T)> {
-        self.data
-            .iter()
-            .find(|(key, set)| allowed_names.contains(key) && !set.is_empty())
-            .and_then(|(&first_name, set)| {
-                set.iter()
-                    .find(|&name| allowed_names.contains(name))
-                    .map(|&second_name| (first_name, second_name))
-            })
-    }
-
-    /// Removes pair from set. Returns `true` if pair was found.
-    ///
-    /// `name_a` should be less than `name_b`.
-    fn remove_pair(&mut self, name_a: T, name_b: T) -> bool {
-        self.data
-            .get_mut(&name_a)
-            .map(|set| set.remove(&name_b))
-            .unwrap_or(false)
-    }
-
-    /// Returns a pair made from `allowed_names` and removes from set.
-    fn pop_pair(&mut self, allowed_names: &Set<T>) -> Option<(T, T)> {
-        let maybe_pair = self.get_pair(allowed_names);
-
-        if let Some((a, b)) = maybe_pair {
-            self.remove_pair(a, b);
-        }
-
-        maybe_pair
-    }
-
-    /// Distributes `allowed_names` to available pairs.
-    ///
-    /// Excludes used names from `allowed_names`. Empty set will be returned if no single pair possible.
-    fn pop_distribution(&mut self, allowed_names: &mut Set<T>) -> Set<(T, T)> {
-        let mut result = Set::new();
-
-        while let Some(pair) = self.pop_pair(allowed_names) {
-            result.insert(pair);
-            let (a, b) = pair;
-            allowed_names.remove(&a);
-            allowed_names.remove(&b);
-        }
-
-        result
-    }
-
-    /// Returns `Some` distribution that uses all allowed names or `None`.
-    fn pop_distribution_strict(&mut self, allowed_names: &Set<T>) -> Option<Set<(T, T)>> {
-        let mut allowed_names_mut = allowed_names.clone();
-
-        let result = self.pop_distribution(&mut allowed_names_mut);
-
-        if allowed_names_mut.is_empty() {
-            Some(result)
-        } else {
-            println!("Failed at: {:?}", result);
-            None
-        }
-    }
+    None
 }
-
-// 
 
 #[cfg(test)]
 mod tests {
-    mod available_pairs {
-        use crate::*;
+    use crate::*;
 
-        #[test]
-        fn new_dont_panic() {
-            AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-        }
+    #[test]
+    fn all_pairs_for_3_names() {
+        assert_eq!(
+            all_pairs(Set::from([1, 2, 3])),
+            vec![
+                (1, 2),
+                (1, 3),
+                (2, 3)
+            ]
+        )
+    }
 
-        #[test]
-        fn count_for_3_names() {
-            let pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(pt.count(), 3);
-        }
-
-        #[test]
-        fn count_for_4_names() {
-            let pt = AvailablePairs::new(&["Alice", "Bob", "Zak", "John"]);
-            assert_eq!(pt.count(), 6);
-        }
-
-        #[test]
-        fn count_for_10_names() {
-            let pt = AvailablePairs::new(&["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]);
-            assert_eq!(pt.count(), 45);
-        }
-
-        #[test]
-        fn get_pair_when_exists() {
-            let pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(
-                pt.get_pair(&Set::from(["Alice", "Bob"])),
-                Some(("Alice", "Bob"))
-            );
-        }
-
-        #[test]
-        fn get_pair_when_impossible() {
-            let pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(pt.get_pair(&Set::from(["Alice", "Jane"])), None);
-        }
-
-        #[test]
-        fn remove_pair_when_exists() {
-            let mut pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(pt.remove_pair("Alice", "Bob"), true);
-        }
-
-        #[test]
-        fn remove_pair_when_impossible() {
-            let mut pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(pt.remove_pair("Alice", "Bob"), true);
-            assert_eq!(pt.remove_pair("Alice", "Bob"), false);
-        }
-
-        #[test]
-        fn pop_pair_when_exists() {
-            let mut pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(
-                pt.pop_pair(&Set::from(["Alice", "Bob"])),
-                Some(("Alice", "Bob"))
-            );
-            assert_eq!(pt.pop_pair(&Set::from(["Alice", "Bob"])), None);
-        }
-
-        #[test]
-        fn pop_pair_when_impossible() {
-            let mut pt = AvailablePairs::new(&["Alice", "Bob", "Zak"]);
-            assert_eq!(pt.pop_pair(&Set::from(["Alice", "Jane"])), None);
-        }
-
-        #[test]
-        fn pop_distribution_when_possible() {
-            let people = ["Alice", "Bob", "Jane", "John"];
-            let mut pt = AvailablePairs::new(&people);
-            assert_eq!(
-                pt.pop_distribution(&mut Set::from(people)),
-                Set::from([
-                    ("Alice", "Bob"),
-                    ("Jane", "John")
-                ])
-            );
-        }
-
-        #[test]
-        fn pop_distribution_returns_unused() {
-            let people = ["Alice", "Bob", "Jane"];
-            let mut pt = AvailablePairs::new(&people);
-            let mut ppl_set = Set::from(people);
-
-            assert_eq!(
-                pt.pop_distribution(&mut ppl_set),
-                Set::from([
-                    ("Alice", "Bob")
-                ])
-            );
-
-            assert_eq!(ppl_set, Set::from(["Jane"]));
-        }
-
-        #[test]
-        fn pop_distribution_strict_when_possible() {
-            let people = ["Alice", "Bob", "Jane", "John"];
-            let mut pt = AvailablePairs::new(&people);
-            assert_eq!(
-                pt.pop_distribution_strict(&Set::from(people)),
-                Some(Set::from([
-                    ("Alice", "Bob"),
-                    ("Jane", "John")
-                ]))
-            );
-        }
-
-        #[test]
-        fn pop_distribution_strict_when_impossible() {
-            let people = ["Alice", "Bob", "Jane"];
-            let mut pt = AvailablePairs::new(&people);
-            let ppl_set = Set::from(people);
-
-            assert_eq!(
-                pt.pop_distribution_strict(&ppl_set),
-                None
-            );
-        }
+    #[test]
+    fn dist_for_4_names() {
+        let names = Set::from([1, 2, 3, 4]);
+        let pairs = all_pairs(names.clone());
+        let pairs_slice = pairs.iter().as_slice();
+        assert_eq!(
+            dist(pairs_slice, names),
+            Some(vec![(3, 4), (1, 2)])
+        )
     }
 }
 
+fn dist2<T: Clone + Ord + Copy>(pairs: Set<(T, T)>, names: Set<T>) -> Option<Vec<(T, T)>> {
+    let pairs_vec = Vec::from_iter(pairs.iter().cloned());
+    let pairs_slice = pairs_vec.iter().as_slice();
+
+    dist(pairs_slice, names)
+}
+
 fn main() {
-    let ppl = ["Dejan", "Edin", "Erik", "Roman", "Iuri", "Reza", "Jonas", "Max", "Moritz", "Tuomo", "Rafael", "NoPair"];
-    let mut ap = AvailablePairs::new(&ppl);
+    let names = Set::from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    let pairs = all_pairs(names.clone());
+    let mut pairs_set = Set::from_iter(pairs.iter().cloned());
 
-    println!("Total uniq pairs: {}", ap.count());
-    println!("---");
+    while let Some(new_dist) = dist2(pairs_set.clone(), names.clone()) {
+        println!("{:?}", new_dist);
 
-    while let Some(dis) = ap.pop_distribution_strict(&Set::from(ppl)) {
-        println!("{:?}", dis);
+        for pair in new_dist {
+            pairs_set.remove(&pair);
+        }
     }
-
-    println!("Pairs left: {:?}", ap);
 }
